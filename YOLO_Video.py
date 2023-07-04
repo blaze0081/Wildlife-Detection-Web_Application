b06204588ad0458e5641518d17b4974feb046973
blaze0081 <bhaskarmishra008@gmail.com>
2023-07-04 13:36:31 +0530

    updates

--- YOLO_Video.py
+++ YOLO_Video.py
@@ -26,12 +26,12 @@ def video_detection(path_x):
                 conf=math.ceil((box.conf[0]*100))/100
                 cls=int(box.cls[0])
                 class_name=classNames[cls]
-                label=f'{class_name}{conf}'
-                t_size = cv2.getTextSize(label, 0, fontScale=1, thickness=2)[0]
+                label=f'{class_name} {conf}'
+                t_size = cv2.getTextSize(label, 0, fontScale=2, thickness=2)[0]
                 print(t_size)
                 c2 = x1 + t_size[0], y1 - t_size[1] - 3
                 cv2.rectangle(img, (x1,y1), c2, [255,0,255], -1, cv2.LINE_AA)  # filled
-                cv2.putText(img, label, (x1,y1-2),0, 1,[255,255,255], thickness=1,lineType=cv2.LINE_AA)
+                cv2.putText(img, label, (x1,y1-2),0, 1,[255,255,255], thickness=2,lineType=cv2.LINE_AA)
         yield img
         #out.write(img)
         #cv2.imshow("image", img)
